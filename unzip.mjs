a8c4fca7ee1cfd8b894b6aaae8c4a20d7a44af71
RbCaViTerminal <robert@robertvail.info>
2023-09-13 20:17:22 -0400

    add unzip.unzip

--- unzip.mjs
+++ unzip.mjs
@@ -6,38 +6,45 @@ import * as path from 'node:path';
 
 import * as yauzl from 'yauzl';
 
-let downloadunzip=(url,contentroot,dest)=>download(url).then(buffer=>yauzl.fromBuffer(buffer, {lazyEntries: true}, function(err, zipfile) {
-  if (err) throw err;
-  let ncroot=path.normalize(contentroot);
-  let contentrootlength=ncroot.split(path.sep).length;
-  if(ncroot=='.'){
-    ncroot='';
-    contentrootlength=0;
-  }
-  zipfile.readEntry();
-  zipfile.on("entry", function(entry) {
-    if (/\/$/.test(entry.fileName)) {
-      zipfile.readEntry();
-    } else {
-      console.log(path.normalize(entry.fileName),ncroot);
-      if(!path.normalize(entry.fileName).startsWith(ncroot)){
-        zipfile.readEntry();
-        return;
-      }
-      const pathComponents = path.normalize(entry.fileName).split(path.sep);
-      const fname=path.join(...pathComponents.slice(contentrootlength));
-      // file entry
-      zipfile.openReadStream(entry, function(err, readStream) {
-        if (err) throw err;
-        readStream.on("end", function() {
-          zipfile.readEntry();
-        });
-        fs.mkdirSync(path.join(dest,path.dirname(fname)), { recursive: true });
-        const stream=fs.createWriteStream(path.join(dest,fname));
-        readStream.pipe(stream);
-      });
+function unzipcallback(contentroot,dest){
+  function callback(err, zipfile) {
+    if (err) throw err;
+    let ncroot=path.normalize(contentroot);
+    let contentrootlength=ncroot.split(path.sep).length;
+    if(ncroot=='.'){
+      ncroot='';
+      contentrootlength=0;
     }
-  });
-}));
+    zipfile.readEntry();
+    zipfile.on("entry", function(entry) {
+      if (/\/$/.test(entry.fileName)) {
+        zipfile.readEntry();
+      } else {
+        console.log(path.normalize(entry.fileName),ncroot);
+        if(!path.normalize(entry.fileName).startsWith(ncroot)){
+          zipfile.readEntry();
+          return;
+        }
+        const pathComponents = path.normalize(entry.fileName).split(path.sep);
+        const fname=path.join(...pathComponents.slice(contentrootlength));
+        // file entry
+        zipfile.openReadStream(entry, function(err, readStream) {
+          if (err) throw err;
+          readStream.on("end", function() {
+            zipfile.readEntry();
+          });
+          fs.mkdirSync(path.join(dest,path.dirname(fname)), { recursive: true });
+          const stream=fs.createWriteStream(path.join(dest,fname));
+          readStream.pipe(stream);
+        });
+      }
+    });
+  }
+  return callback;
+}
 
-export {downloadunzip};
+let downloadunzip=(url,contentroot,dest)=>download(url).then(buffer=>yauzl.fromBuffer(buffer, {lazyEntries: true}, unzipcallback(contentroot,dest)));
+
+let unzip=(filename,contentroot,dest)=>yauzl.open(filename, {lazyEntries: true}, unzipcallback(contentroot,dest))
+
+export {downloadunzip,unzip};
