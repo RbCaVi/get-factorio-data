a6816fd9787e03c62119bec7354414cfb16299bc
RbCaViTerminal <robert@robertvail.info>
2023-09-13 21:37:17 -0400

    import packages

--- resolvedeps2.mjs
+++ resolvedeps2.mjs
@@ -1,3 +1,7 @@
+import * as fsPromises from 'node:fs/promises';
+import * as path from 'node:path';
+import * as os from 'node:os';
+
 import {downloadunzip} from './unzip.mjs'
 
 import {versionConstraint,incompatible,anyVersion,constraint,VersionConstraint} from './version.mjs';
