a0dd00d24c1c10418c82d37cd7bc1d3f2c3ef2e8
RbCaViTerminal <robert@robertvail.info>
2023-09-13 20:16:51 -0400

    add downloadfile

--- download.mjs
+++ download.mjs
@@ -20,4 +20,22 @@ const download=(url,asString=false)=>new Promise((resolve,reject)=>
   }).on('error', (err) => { reject(['download error:', err]); })
 );
 
-export {download};
+const downloadfile=(url,filename)=>new Promise((resolve,reject)=>
+  https.get(url, (res) => {
+    const file=fs.createWriteStream(filename);
+    const data = [];
+    res.pipe(file);
+    res.on('end', () => {
+      if (!res.complete){
+        reject('The connection was terminated while the message was still being sent');
+      }
+    }).on('error', () => {
+      file.end();
+    });
+    file.on('end',()=>{
+      resolve();
+    });
+  }).on('error', (err) => { reject(['download error:', err]); })
+);
+
+export {download,downloadfile};
