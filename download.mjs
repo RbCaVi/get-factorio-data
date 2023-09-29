105998b441b8526815ebde7dce02a2175aff2a06
RbCaViTerminal <robert@robertvail.info>
2023-09-29 09:01:02 -0400

    add request back in

--- download.mjs
+++ download.mjs
@@ -1,5 +1,19 @@
 import * as https from 'node:https';
 
+let headers={'User-Agent':'RbCaVi-SEJS'}
+
+const request=url=>new Promise((resolve,reject)=>{
+  https.get(url,{headers:headers},resolve).on('error', reject)
+});
+
+const requestRedirect=url=>request(url).then(res=>{
+  if(res.statusCode==301||res.statusCode==302||res.statusCode==307||res.statusCode==308){
+    console.log('redirected',url,res.statusCode,res.headers.location)
+    return requestRedirect(res.headers.location);
+  }
+  return res;
+})
+
 const download=(url,asString=false)=>new Promise((resolve,reject)=>
   requestRedirect(url).then((res) => {
     const data = [];
