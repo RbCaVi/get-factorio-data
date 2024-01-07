1ee18c0348e079b6d384b7d29eb8c5a24bf4b0a7
RbCaViTerminal <robert@robertvail.info>
2024-01-07 15:47:40 -0500

    fix a bunch of things with eslint

--- process.js
+++ process.js
@@ -1,15 +1,15 @@
 /* jshint esversion: 11 */
-import * as fsPromises from 'node:fs/promises';
-import * as fs from 'node:fs';
-import * as path from 'node:path';
-import * as child_process from 'node:child_process';
+import * as fsPromises from "node:fs/promises";
+import * as fs from "node:fs";
+import * as path from "node:path";
+import * as child_process from "node:child_process";
 
-import * as file from './file.mjs';
-import * as download from './download.mjs';
-import * as retry from './retry.mjs';
-import * as unzip from './unzip.mjs';
+import * as file from "./file.mjs";
+import * as download from "./download.mjs";
+import * as retry from "./retry.mjs";
+import * as unzip from "./unzip.mjs";
 
-import {versionConstraint,anyVersion} from './version.mjs';
+import {versionConstraint,anyVersion} from "./version.mjs";
 
 //version is an upper and lower bound
 //inclusive or exclusive
@@ -30,17 +30,17 @@ async function resolveAllMap(ps){
   return values;
 }
 
-function geturl(mod,rversion,data){
+function geturl(mod,rversion,/*data*/){
   const version=rversion.version;
   //console.log(mod,version,data);
   let url,contentroot;
 
-  if(mod=='core'||mod=='base'){
+  if(mod=="core"||mod=="base"){
     url=`https://api.github.com/repos/wube/factorio-data/zipball/${rversion.ref}`;
-    contentroot=`wube-factorio-data-${(''+rversion.ref).slice(0,7)}/${mod}`;
+    contentroot=`wube-factorio-data-${(""+rversion.ref).slice(0,7)}/${mod}`;
   }else{
     url=`https://mods-storage.re146.dev/${mod}/${version}.zip`;
-    contentroot='';//`${mod}_${version}`;
+    contentroot="";//`${mod}_${version}`;
   }
   const dest=`mods/${mod}-${version}`;
 
@@ -53,31 +53,31 @@ async function run(pack){
   for(const [mod,moddata] of Object.entries(pack.mods)){
     let version;
     if(moddata.version){
-      version=versionConstraint('____ = '+moddata.version,mod,'__initial__');
+      version=versionConstraint("____ = "+moddata.version,mod,"__initial__");
     }else{
-      version=anyVersion(mod,'__initial__');
+      version=anyVersion(mod,"__initial__");
     }
     versions.set(mod,version);
   }
 
   // make core and base share the same version
   let coreversion,baseversion;
-  if(!versions.has('core')){
-    coreversion=anyVersion('core','__initial__');
+  if(!versions.has("core")){
+    coreversion=anyVersion("core","__initial__");
   }else{
-    coreversion=versions.get('core');
+    coreversion=versions.get("core");
   }
-  if(!versions.has('base')){
-    baseversion=anyVersion('base','__initial__');
+  if(!versions.has("base")){
+    baseversion=anyVersion("base","__initial__");
   }else{
-    baseversion=versions.get('base');
+    baseversion=versions.get("base");
   }
-  coreversion.mod='core+base';
-  baseversion.mod='core+base';
+  coreversion.mod="core+base";
+  baseversion.mod="core+base";
   coreversion.intersect(baseversion);
   const mergedversion=coreversion;
-  versions.set('base',mergedversion);
-  versions.set('core',mergedversion);
+  versions.set("base",mergedversion);
+  versions.set("core",mergedversion);
   //versions.set('core+base',mergedversion);
 
 
@@ -86,17 +86,17 @@ async function run(pack){
     resolvedVersions.set(mod,version.resolve());
   }
 
-  resolvedVersions=await resolveAllMap(resolvedVersions);
+  const resolved2Versions=await resolveAllMap(resolvedVersions);
 
   const errors=[];
-  for(const [,resolvedVersion] of resolvedVersions){
+  for(const [,resolvedVersion] of resolved2Versions){
     for(const [depmod,depversion] of resolvedVersion.deps){
-      if(!depversion.incompatible&&!depversion.optional&&!resolvedVersions.has(depmod)){
+      if(!depversion.incompatible&&!depversion.optional&&!resolved2Versions.has(depmod)){
         errors.push(`Unresolved dependency: ${depmod} - needs version ${depversion}`);
       }
-      if(depversion.incompatible&&depversion.includes(resolvedVersions.get(depmod)?.version)){
-        if(depmod=='base'){
-          resolvedVersions.delete('base');
+      if(depversion.incompatible&&depversion.includes(resolved2Versions.get(depmod)?.version)){
+        if(depmod=="base"){
+          resolved2Versions.delete("base");
         }else{
           errors.push(`Incompatible version: ${depmod} - needs version ${depversion}`);
         }
@@ -109,15 +109,15 @@ async function run(pack){
 
   const modlocations=[];
   for(const [mod,resolvedVersion] of resolvedVersions){
-    console.log(mod, 'resolves to',resolvedVersion);
+    console.log(mod, "resolves to",resolvedVersion);
     const location=geturl(mod,resolvedVersion,pack.mods[mod]);
     modlocations.push([...location,mod,resolvedVersion.version]);
   }
-  console.log('downloading',modlocations);
+  console.log("downloading",modlocations);
   return modlocations;
 }
 
-const packdata=await file.read('pack.json');
+const packdata=await file.read("pack.json");
 console.log(packdata);
 const pack=JSON.parse(packdata);
 const modlocations=await run(pack);
@@ -125,7 +125,7 @@ const modlocations=await run(pack);
 //await file.write('modlocations.json',modlocationsdata);
 
 
-const factorioroot=await file.read('factorioroot.txt').trim();
+const factorioroot=await file.read("factorioroot.txt").trim();
 
 
 // https://stackoverflow.com/a/63497965
@@ -148,7 +148,7 @@ for(const l of modlocations){
 let coreversion;
 
 for(const [url,v] of groupedmods){
-  const temp=$(mktemp) // get temp file name
+  const temp=$(mktemp); // get temp file name
   retry.retryifyAsync(download.downloadToFile)(url,temp);
   for(const [,unzipto,vroot,mod,version] of v){
     let root=vroot;
@@ -166,13 +166,13 @@ for(const [url,v] of groupedmods){
       root=defaultroot;
     }
     if(mod=="base"){
-      await fsPromises.mkdir(path.join(unzipto,root,'menu-simulations'));
+      await fsPromises.mkdir(path.join(unzipto,root,"menu-simulations"));
       await fsPromises.copyFile(
-        path.join(factorioroot,'menu-simulations/menu-simulations.lua'),
-        path.join(unzipto,root,'menu-simulations/menu-simulations.lua')
+        path.join(factorioroot,"menu-simulations/menu-simulations.lua"),
+        path.join(unzipto,root,"menu-simulations/menu-simulations.lua")
       );
     }
-    if(mod=='core'){
+    if(mod=="core"){
       coreversion=version;
     }
   }
@@ -193,24 +193,44 @@ for(const [,unzipto,root,mod,version] of modlocations){
 
 const writestream=fs.createWriteStream("fdata.lua");
 
-try{
-  // first try json
-  const defines=await import('defines.json');
+function writedefinesfromjson(defines,prefix,writestream) {
+  writestream.write(`${prefix}={}\n`);
+  for(const [name,value] of defines){
+    const subprefix=prefix+"."+name;
+    if(typeof value=="object"){
+      writedefinesfromjson(value,subprefix,writestream);
+    }else{
+      writestream.write(`${subprefix}=value\n`);
+    }
+  }
+}
 
-  function writedefines(defines,prefix,writestream) {
-    writestream.write(`${prefix}={}\n`);
-    for(const [name,value] of defines){
-      const subprefix=prefix+"."+name;
-      if(typeof value=='object'){
-        writedefines(value,subprefix,writestream);
-      }else{
-        writestream.write(`${subprefix}=value\n`);
+function writedefinesfromapi(defines,prefix,writestream) {
+  for(const define of defines){
+    const subprefix=prefix+"."+define.name;
+    writestream.write(`${subprefix}={}\n`);
+    if("values" in define){
+      for(const {name} of define.values){
+        const valuename=subprefix+"."+name;
+        writestream.write(`${valuename}="${valuename}"\n`);
+        // like i could do it in numeric order like the actual defines
+        // but there are special cases i don't want to handle
+        // this should be just a fallback
+      }
+    }else if("subkeys" in define){
+      for(const subdefines of define.subkeys){
+        writedefinesfromapi(subdefines,subprefix,writestream);
       }
     }
   }
+}
+
+try{
+  // first try json
+  const defines=await import("defines.json");
 
   writestream.write("local ");
-  writedefines(defines.defines,"defines",writestream);
+  writedefinesfromjson(defines.defines,"defines",writestream);
   writestream.write("\n");
 }catch{
   try{
@@ -222,31 +242,10 @@ try{
     writestream.write("\n");
   }catch{
     // finally fall back to taking it from the factorio api
-    const runtimeapi=download.downloadjson(`https://lua-api.factorio.com/${version}/runtime-api.json`);
-
-    // defines is list
-    function writedefines(defines,prefix,writestream) {
-      for(const define of defines){
-        const subprefix=prefix+"."+define.name;
-        writestream.write(`${subprefix}={}\n`);
-        if("values" in define){
-          for(const {name} of define.values){
-            const valuename=subprefix+"."+name;
-            writestream.write(`${valuename}="${valuename}"\n`);
-            // like i could do it in numeric order like the actual defines
-            // but there are special cases i don't want to handle
-            // this should be just a fallback
-          }
-        }else if("subkeys" in define){
-          for(const subdefines of define.subkeys){
-            writedefines(subdefines,subprefix,writestream);
-          }
-        }
-      }
-    }
+    const runtimeapi=download.downloadjson(`https://lua-api.factorio.com/${coreversion}/runtime-api.json`);
 
     writestream.write("local defines={}\n");
-    writedefines(runtimeapi.defines,"defines",writestream);
+    writedefinesfromapi(runtimeapi.defines,"defines",writestream);
     writestream.write("\n");
   }
 }
@@ -261,34 +260,34 @@ function writetypes(data,writestream) {
   writestream.write("}");
 }
 
-let res=await download.request(`https://lua-api.factorio.com/${version}/prototype-api.json`);
+let res=await download.request(`https://lua-api.factorio.com/${coreversion}/prototype-api.json`);
 if(!(res.statusCode>=200&&res.statusCode<300)){
   res=await download.request("https://lua-api.factorio.com/latest/prototype-api.json");
 }
 
 const parts=[];
 const data=await new Promise((resolve,reject)=>
-  res.on('data', (chunk) => {
+  res.on("data", (chunk) => {
     parts.push(chunk);
-  }).on('end', () => {
+  }).on("end", () => {
     if (!res.complete){
-      reject('The connection was terminated while the message was still being sent');
+      reject("The connection was terminated while the message was still being sent");
     }
-    const string = parts.join('');
+    const string = parts.join("");
     resolve(string);
   })
 );
 
 writetypes(JSON.parse(data),writestream);
 
-writestream.write('\nreturn {defines=defines,types=datatypes}');
+writestream.write("\nreturn {defines=defines,types=datatypes}");
 writestream.close();
 
 
 // generate data.json
 const savedluapath=process.env.LUA_PATH;
 
-child_process.spawn('lua',[`${__dirname}/gen.lua`],{stdio:'ignore'});
+child_process.spawn("lua",[`${__dirname}/gen.lua`],{stdio:"ignore"});
 process.env.LUA_PATH=savedluapath;
 
 // https://stackoverflow.com/a/45130990
@@ -305,11 +304,11 @@ async function* getFiles(dir,basePath=".") {
   }
 }
 
-modlocations.map(([,unzipto,root,mod,version])=>({
+modlocations.map(([,,,mod,version])=>({
   mod,
   version,
   modroot:(mod=="base"||mod=="core")?`${factorioroot}/data/$(mod)`:modroots[mod]
-})).map(({mod,version,modroot})=>{
+})).map(async ({mod,version,modroot})=>{
   const outdir=`assets/${mod}_${version}`;
   fs.mkdir(outdir,{recursive:true});
   for await(const name of getFiles(modroot)){
@@ -332,10 +331,10 @@ const localefiles={};
 
 for(const [mod,croot] of Object.entries(modroots)){
   let root=croot;
-  if(mod=='base'||mod=='core'){ // get locale from an installed factorio because they're not in factorio-data
-    root=path.join(factorioroot,'data',mod);
+  if(mod=="base"||mod=="core"){ // get locale from an installed factorio because they're not in factorio-data
+    root=path.join(factorioroot,"data",mod);
   }
-  const localedir = await fsPromises.opendir(path.join(root,'locale'));
+  const localedir = await fsPromises.opendir(path.join(root,"locale"));
   for await (const entry of localedir){
     if(!entry.isDirectory()){
       continue;
@@ -344,10 +343,10 @@ for(const [mod,croot] of Object.entries(modroots)){
     if(!(lang in localefiles)){
       localefiles[lang]=[];
     }
-    const localedir = await fsPromises.opendir(path.join(root,'locale',lang));
+    const localedir = await fsPromises.opendir(path.join(root,"locale",lang));
     for await (const entry of localedir){
-      if(entry.name.endsWith('.cfg')){
-        localefiles[lang].push(path.join(root,'locale',lang,entry.name));
+      if(entry.name.endsWith(".cfg")){
+        localefiles[lang].push(path.join(root,"locale",lang,entry.name));
       }
     }
   }
@@ -365,17 +364,17 @@ for(const [lang,langfiles] of Object.entries(localefiles)){
     let category;
     for(const line of data.split(/\r?\n/)){
       const trimmedline=line.trim();
-      if(trimmedline==''||trimmedline.startsWith(';')||trimmedline.startsWith('#')){
+      if(trimmedline==""||trimmedline.startsWith(";")||trimmedline.startsWith("#")){
         continue;
       }
-      if(trimmedline.startsWith('[')&&trimmedline.endsWith(']')){
+      if(trimmedline.startsWith("[")&&trimmedline.endsWith("]")){
         category=trimmedline.slice(1,-1);
       }else{
-        const [key,value]=trimmedline.split('=',2);
+        const [key,value]=trimmedline.split("=",2);
         if(!category){
           locale[lang][key]=value;
         }else{
-          locale[lang][category+'.'+key]=value;
+          locale[lang][category+"."+key]=value;
         }
       }
     }
@@ -386,6 +385,6 @@ for(const [lang,langfiles] of Object.entries(localefiles)){
 //await file.write('locale.json',localedata);
 
 
-mkdir "$output"
+//mkdir "$output"
 
-mv assets data.json locale.json "$output"
+//mv assets data.json locale.json "$output"
