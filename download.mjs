45a46929b8b75d55f40cdcc328c5c0bfd75fdae9
RbCaViTerminal <robert@robertvail.info>
2024-01-07 18:12:02 -0500

    fix some more things with eslint

--- download.mjs
+++ download.mjs
@@ -1,31 +1,31 @@
-import * as https from 'node:https';
-import * as fs from 'node:fs';
+import * as https from "node:https";
+import * as fs from "node:fs";
 
-let headers={'User-Agent':'RbCaVi-SEJS'}
+let headers={"User-Agent":"RbCaVi-SEJS"};
 
 const request=url=>new Promise((resolve,reject)=>{
-  https.get(url,{headers:headers},resolve).on('error', reject)
+  https.get(url,{headers:headers},resolve).on("error", reject);
 });
 
 const requestRedirect=url=>request(url).then(res=>{
   if(res.statusCode==301||res.statusCode==302||res.statusCode==307||res.statusCode==308){
-    console.log('redirected',url,res.statusCode,res.headers.location)
+    console.log("redirected",url,res.statusCode,res.headers.location);
     return requestRedirect(res.headers.location);
   }
   return res;
-})
+});
 
 const download=(url,asString=false)=>new Promise((resolve,reject)=>
   requestRedirect(url).then((res) => {
     const data = [];
-    res.on('data', (chunk) => {
+    res.on("data", (chunk) => {
       data.push(chunk);
-    }).on('end', () => {
+    }).on("end", () => {
       if (!res.complete){
-        reject('The connection was terminated while the message was still being sent');
+        reject("The connection was terminated while the message was still being sent");
       }
       if(asString){
-        const string = data.join('');
+        const string = data.join("");
         resolve(string);
       }else{
         const buffer = Buffer.concat(data);
@@ -35,13 +35,13 @@ const download=(url,asString=false)=>new Promise((resolve,reject)=>
   },reject)
 );
 
-const downloadToFile=(url,filename,asString=false)=>new Promise((resolve,reject)=>
+const downloadToFile=(url,filename)=>new Promise((resolve,reject)=>
   requestRedirect(url).then((res) => {
     const file = fs.createWriteStream(filename);
     res.pipe(file);
-    file.on('finish', () => {
+    file.on("finish", () => {
       if (!res.complete){
-        reject('The connection was terminated while the message was still being sent');
+        reject("The connection was terminated while the message was still being sent");
       }
       resolve();
     });
@@ -50,4 +50,4 @@ const downloadToFile=(url,filename,asString=false)=>new Promise((resolve,reject)
 
 const downloadjson=(url)=>download(url,true).then(data=>JSON.parse(data));
 
-export {download,downloadjson};
+export {request,requestRedirect,download,downloadjson,downloadToFile};
--- file.mjs
+++ file.mjs
@@ -1,17 +1,17 @@
-import * as fs from 'node:fs';
+import * as fs from "node:fs";
 
 let read=filename=>new Promise((resolve,reject)=>{
-  let s='';
+  let s="";
   let f=fs.createReadStream(filename);
-  f.on('data',data=>{s+=data;});
-  f.on('end',()=>{resolve(s)});
-  f.on('error',reject);
+  f.on("data",data=>{s+=data;});
+  f.on("end",()=>{resolve(s);});
+  f.on("error",reject);
 });
 
-let write=(filename,data)=>new Promise((resolve,reject)=>{
-  let s='';
+let write=(filename,data)=>new Promise((resolve,)=>{
   let f=fs.createWriteStream(filename);
   f.write(data,resolve);
+  f.close();
 });
 
 export {read,write};
