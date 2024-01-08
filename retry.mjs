60ccbed40544902e432d8b00210c21b4f12b258b
RbCaViTerminal <robert@robertvail.info>
2024-01-07 19:10:15 -0500

    fix retryifyAsync

--- retry.mjs
+++ retry.mjs
@@ -1,5 +1,5 @@
 const Y=h=>(...args)=>h(h,...args);
 const retryify=f=>Y((g,...args)=>f(...args).catch(g(g,...args)));
-const retryifyAsync=f=>Y((g,...args)=>(await f(...args)).catch(g(g,...args)));
+const retryifyAsync=f=>Y(async (g,...args)=>(await f(...args)).catch(g(g,...args)));
 
 export {retryify,retryifyAsync};
